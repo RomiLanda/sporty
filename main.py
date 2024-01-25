f08686b6662768321fd885d641d4fa45207ad337
reptar <romailanda@gmail.com>
2024-01-25 17:15:51 -0300

    updata main with pose

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import cv2
 import mediapipe as mp
 from src.exercises import Exercises
 from src.utils import *
-
+from src.angle_body_part import BodyPartDistance
 
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-EXERCISE = "pull_up"
+EXERCISE = "squat"
+VIDEO = "input/squat.mp4"
 
 def video_analyzer():
-    cap = cv2.VideoCapture("input/pull-up.mp4")
+    cap = cv2.VideoCapture(VIDEO)
 
     # Initialize holistic model 
     holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
     
     counter = 0  # movement of exercise
     status = True  # state of move
+    pose = ""
         
     while cap.isOpened():
         status_video, frame = cap.read()
@@ -32,9 +34,12 @@ def video_analyzer():
         results = holistic.process(rgb_frame)
         
         landmarks = results.pose_landmarks.landmark
+
+
+        counter, status, pose = Exercises(landmarks=landmarks).calculate_exercise(EXERCISE, counter, status, pose)
         
-        counter, status = Exercises(landmarks=landmarks).calculate_exercise(EXERCISE, counter, status)
-        print(counter, status)
+        print(counter, status, pose)
+
 
         # Pose
         mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
